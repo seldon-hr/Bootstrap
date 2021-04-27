8d3b04748d9f679c4fc53ab86e59c4fd45942f5a
Daniel Romero <daanmaro99june@gmail.com>
2021-04-27 13:14:12 -0500

    Ajustes de funciones

---
+++ test.js
@@ -0,0 +1,41 @@
+function Show() {
+    document.getElementById("menu").style.display="block";
+}
+
+function Hide() {
+    document.getElementById("menu").style.display="none";
+}
+
+
+function Show2() {
+    document.getElementById("menu2").style.display="block";
+}
+
+function Hide2() {
+    document.getElementById("menu2").style.display="none";
+}
+
+function dropdown2() {
+    var caja = document.getElementById("menu2");
+    if(caja.style.display == "none"){
+        Show2();
+        document.getElementById("divulgacionWord2").style.position = "absolute";
+    }
+    else{
+        Hide2();
+        document.getElementById("divulgacionWord2").style.position = "relative";
+}
+}
+
+function dropdown() {
+    var caja = document.getElementById("menu");
+    if(caja.style.display == "none"){
+        Show();
+        document.getElementById("divulgacionWord").style.position = "absolute";
+    }
+    else{
+        Hide();
+        document.getElementById("divulgacionWord").style.position = "relative";
+}
+}
+
